08751530308af1fa1ff322d8d5274a0f733757ca
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-03-05 14:37:24 +0200

    Refactor

--- il2ds_difficulty/constants.py
+++ il2ds_difficulty/constants.py
@@ -2,6 +2,16 @@
 """
 Difficulty constants.
 """
+try:
+    from django.utils.translation import ugettext as _
+except ImportError:
+    def _(value):
+        return value
+
+# -----------------------------------------------------------------------------
+# All settings codes
+# -----------------------------------------------------------------------------
+
 WIND_TURBULENCE = 'WindTurbulence'
 FLUTTER_EFFECT = 'FlutterEffect'
 STALL_SPINS = 'StallSpins'
@@ -41,6 +51,79 @@ NO_FRIENDLY_VIEW = 'NoFriendlyView'
 NO_PLANES_VIEW = 'NoPlanesView'
 NO_AIRCRAFT_CARRIER_VIEW = 'NoACarrierView'
 
+# -----------------------------------------------------------------------------
+# All settings names and descriptions
+# -----------------------------------------------------------------------------
+
+SETTINGS_NAMES_MAP = {
+    WIND_TURBULENCE: (
+        _("Wind and turbulence"),
+        _("Description"),
+    ),
+    FLUTTER_EFFECT: ("Flutter", "Description"),
+    STALL_SPINS: ("Tailspin", "Description"),
+    BLACKOUTS_REDOUTS: ("Overload", "Description"),
+    ENGINE_OVERHEAT: ("Engine overheating", "Description"),
+    TORQUE_GYRO_EFFECTS: ("Gyroscopic moment", "Description"),
+    REALISTIC_LANDING: ("Realistic landing", "Description"),
+    TAKEOFF_LANDING: ("Take-off and landing", "Description"),
+    COCKPIT_ALWAYS_ON: ("View only from the cockpit", "Description"),
+    NO_OUTSIDE_VIEWS: ("No outside views", "Description"),
+    HEAD_SHAKE: ("Moving head", "Description"),
+    NO_ICONS: ("Without icons players", "Description"),
+    REALISTIC_GUNNERY: ("Realistic shooting", "Description"),
+    LIMITED_AMMO: ("Limited ammunition", "Description"),
+    LIMITED_FUEL: ("Limited supply of fuel", "Description"),
+    VULNERABILITY: ("Vulnerability", "Description"),
+    NO_PADLOCK: ("Disable glance fixation", "Description"),
+    CLOUDS: ("Clouds", "Description"),
+    NO_MAP_ICONS: ("Disable the markers on the map", "Description"),
+    SEPARATE_ENGINE_START: ("Separate engine starting", "Description"),
+    NO_INSTANT_SUCCESS: ("Complete the task", "Description"),
+    NO_MINIMAP_PATH: ("Without the route on the map", "Description"),
+    NO_SPEED_BAR: ("Without speed indicator", "Description"),
+    COMPLEX_ENGINE_MANAGEMENT: ("Full engine control", "Description"),
+    RELIABILITY: ("Real reliability of engines", "Description"),
+    OVERLOAD_LIMITS: ("Dynamic overload restriction", "Description"),
+    REALISTIC_PILOT_VULNERABILITY: ("Realistic vulnerability pilot", "Description"),
+    REALISTIC_NAVIGATION_INSTRUMENTS: ("Realistic navigation", "Description"),
+    NO_PLAYER_ICON: ("To disable a player on the map", "Description"),
+    NO_FOG_OF_WAR_ICONS: ("Disable icons intelligence on the map", "Description"),
+    BOMB_FUZES: ("Bomb fuses", "Description"),
+    REALISTIC_TORPEDOING: ("Realistic torpedoing", "Description"),
+    REALISTIC_MISSILES_VARIATION: ("Realistic spread of missiles", "Description"),
+    NO_SELF_VIEW: ("Without your own review", "Description"),
+    NO_FOE_VIEW: ("Without a review of the enemy", "Description"),
+    NO_FRIENDLY_VIEW: ("Without friends", "Description"),
+    NO_PLANES_VIEW: ("Without a review of the aircraft", "Description"),
+    NO_AIRCRAFT_CARRIER_VIEW: ("Without a review of aircraft carriers", "Description"),
+}
+
+# -----------------------------------------------------------------------------
+# All tabs codes
+# -----------------------------------------------------------------------------
+
+TAB_FLIGHT_MODEL = 'flight_model'
+TAB_WEAPONS = 'weapons'
+TAB_VIEW = 'view'
+TAB_ICONS_N_MAP = 'icons_n_map'
+TAB_MISC = 'misc'
+
+# -----------------------------------------------------------------------------
+# All tabs names
+# -----------------------------------------------------------------------------
+
+TABS_NAMES_MAP = {
+    TAB_FLIGHT_MODEL: _("Flight model"),
+    TAB_WEAPONS: _("Weapons"),
+    TAB_VIEW: _("View"),
+    TAB_ICONS_N_MAP: _("Icons and map"),
+    TAB_MISC: _("Miscellaneous"),
+}
+
+# -----------------------------------------------------------------------------
+# Settings set for 4.12
+# -----------------------------------------------------------------------------
 
 NUMBERS_MAP_4_12 = {
     WIND_TURBULENCE: 0,
@@ -83,49 +166,9 @@ NUMBERS_MAP_4_12 = {
     NO_AIRCRAFT_CARRIER_VIEW: 37,
 }
 
-NAME_MAP = {
-    WIND_TURBULENCE: ('Wind and turbulence', 'Description'),
-    FLUTTER_EFFECT: ('Flutter', 'Description'),
-    STALL_SPINS: ('Tailspin', 'Description'),
-    BLACKOUTS_REDOUTS: ('Overload', 'Description'),
-    ENGINE_OVERHEAT: ('Engine overheating', 'Description'),
-    TORQUE_GYRO_EFFECTS: ('Gyroscopic moment', 'Description'),
-    REALISTIC_LANDING: ('Realistic landing', 'Description'),
-    TAKEOFF_LANDING: ('Take-off and landing', 'Description'),
-    COCKPIT_ALWAYS_ON: ('View only from the cockpit', 'Description'),
-    NO_OUTSIDE_VIEWS: ('No outside views', 'Description'),
-    HEAD_SHAKE: ('Moving head', 'Description'),
-    NO_ICONS: ('Without icons players', 'Description'),
-    REALISTIC_GUNNERY: ('Realistic shooting', 'Description'),
-    LIMITED_AMMO: ('Limited ammunition', 'Description'),
-    LIMITED_FUEL: ('Limited supply of fuel', 'Description'),
-    VULNERABILITY: ('Vulnerability', 'Description'),
-    NO_PADLOCK: ('Disable glance fixation', 'Description'),
-    CLOUDS: ('Clouds', 'Description'),
-    NO_MAP_ICONS: ('Disable the markers on the map', 'Description'),
-    SEPARATE_ENGINE_START: ('Separate engine starting', 'Description'),
-    NO_INSTANT_SUCCESS: ('Complete the task', 'Description'),
-    NO_MINIMAP_PATH: ('Without the route on the map', 'Description'),
-    NO_SPEED_BAR: ('Without speed indicator', 'Description'),
-    COMPLEX_ENGINE_MANAGEMENT: ('Full engine control', 'Description'),
-    RELIABILITY: ('Real reliability of engines', 'Description'),
-    OVERLOAD_LIMITS: ('Dynamic overload restriction', 'Description'),
-    REALISTIC_PILOT_VULNERABILITY: ('Realistic vulnerability pilot', 'Description'),
-    REALISTIC_NAVIGATION_INSTRUMENTS: ('Realistic navigation', 'Description'),
-    NO_PLAYER_ICON: ('To disable a player on the map', 'Description'),
-    NO_FOG_OF_WAR_ICONS: ('Disable icons intelligence on the map', 'Description'),
-    BOMB_FUZES: ('Bomb fuses', 'Description'),
-    REALISTIC_TORPEDOING: ('Realistic torpedoing', 'Description'),
-    REALISTIC_MISSILES_VARIATION: ('Realistic spread of missiles', 'Description'),
-    NO_SELF_VIEW: ('Without your own review', 'Description'),
-    NO_FOE_VIEW: ('Without a review of the enemy', 'Description'),
-    NO_FRIENDLY_VIEW: ('Without friends', 'Description'),
-    NO_PLANES_VIEW: ('Without a review of the aircraft', 'Description'),
-    NO_AIRCRAFT_CARRIER_VIEW: ('Without a review of aircraft carriers', 'Description'),
-}
 
 TABS_4_12 = {
-    'FLIGHT': (
+    TAB_FLIGHT_MODEL: (
         WIND_TURBULENCE,
         FLUTTER_EFFECT,
         STALL_SPINS,
@@ -134,15 +177,9 @@ TABS_4_12 = {
         TORQUE_GYRO_EFFECTS,
         REALISTIC_LANDING,
         TAKEOFF_LANDING,
-        OVERLOAD_LIMITS
+        OVERLOAD_LIMITS,
     ),
-    'ENGINE': (
-        ENGINE_OVERHEAT,
-        SEPARATE_ENGINE_START,
-        COMPLEX_ENGINE_MANAGEMENT,
-        RELIABILITY
-    ),
-    'VIEWS': (
+    TAB_VIEW: (
         COCKPIT_ALWAYS_ON,
         NO_OUTSIDE_VIEWS,
         HEAD_SHAKE,
@@ -151,9 +188,9 @@ TABS_4_12 = {
         NO_FOE_VIEW,
         NO_FRIENDLY_VIEW,
         NO_PLANES_VIEW,
-        NO_AIRCRAFT_CARRIER_VIEW
+        NO_AIRCRAFT_CARRIER_VIEW,
     ),
-    'REALISM': (
+    TAB_MISC: (
         NO_ICONS,
         REALISTIC_GUNNERY,
         LIMITED_AMMO,
@@ -170,14 +207,27 @@ TABS_4_12 = {
         NO_FOG_OF_WAR_ICONS,
         BOMB_FUZES,
         REALISTIC_TORPEDOING,
-        REALISTIC_MISSILES_VARIATION
+        REALISTIC_MISSILES_VARIATION,
+    # ),
+    # _('Engine'): (
+        ENGINE_OVERHEAT,
+        SEPARATE_ENGINE_START,
+        COMPLEX_ENGINE_MANAGEMENT,
+        RELIABILITY,
     ),
 }
+
+
+# -----------------------------------------------------------------------------
+# General constants
+# -----------------------------------------------------------------------------
+
 
 NUMBERS_MAPS = {
     '4.12': NUMBERS_MAP_4_12,
     '4.12.1': NUMBERS_MAP_4_12,
     '4.12.2': NUMBERS_MAP_4_12,
 }
+
 
 DEFAULT_GAME_VERSION = '4.12.2'
