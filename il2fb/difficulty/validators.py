f88960e185eb6b0ea73edbd46b0fc4b5786841f0
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-07-20 17:10:00 +0300

    fix integer types for Py3

--- il2fb/difficulty/validators.py
+++ il2fb/difficulty/validators.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
+from six import integer_types
+
 from .utils import translations
 
 _ = translations.ugettext
 
 
 def validate_difficulty(value):
-    if not isinstance(value, (int, long)):
+    if not isinstance(value, integer_types):
         raise TypeError(_("Difficulty is not an integer"))
     if value < 0:
         raise ValueError(_("Difficulty must be a positive integer"))
