14a872837dcf722a5cba5a56aa41bd962e8b0a2e
KAMYHIN <alex01081981*>
2015-06-16 12:23:47 +0300

    remove settings for game patches

--- il2fb/config/difficulty/settings/__init__.py
+++ il2fb/config/difficulty/settings/__init__.py
@@ -3,10 +3,11 @@
 from collections import OrderedDict
 from il2fb.commons import GameVersions
 
-from . import v4_12
+from . import v4_12, v4_13
 
 ALL_INFO = (
     (GameVersions.v4_12, v4_12.SETTINGS, v4_12.RULES, v4_12.PRESETS),
+    (GameVersions.v4_13, v4_13.SETTINGS, v4_13.RULES, v4_13.PRESETS),
 )
 
 SETTINGS = OrderedDict([
