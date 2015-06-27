95af975bc4e84abf6bfaa399e0fb306403608710
Alexander Oblovatniy <oblovatniy@gmail.com>
2015-06-27 17:15:18 +0300

    add doctest for inverse_dict()

--- il2fb/config/difficulty/transforms.py
+++ il2fb/config/difficulty/transforms.py
@@ -2,6 +2,12 @@
 
 
 def inverse_dict(source):
+    """
+    >>> inverse_dict({})
+    {}
+    >>> inverse_dict({'one': 1, 'two': 2, })
+    {1: 'one', 2: 'two'}
+    """
     return {v: k for k, v in source.items()}
 
 
