2608f4e6088e561aa220d8d65c4ef3b808d4a259
Alexander Oblovatniy <oblovatniy@gmail.com>
2015-06-28 12:45:40 +0300

    add doctest for flatten_settings()

--- il2fb/config/difficulty/transforms.py
+++ il2fb/config/difficulty/transforms.py
@@ -3,6 +3,8 @@
 
 def inverse_dict(source):
     """
+    Swap keys and values in a dict.
+
     >>> inverse_dict({})
     {}
     >>> inverse_dict({'one': 1, 'two': 2, })
@@ -12,4 +14,23 @@ def inverse_dict(source):
 
 
 def flatten_settings(settings):
+    """
+    Combine all subdictionaries into a single one.
+
+    >>> settings = {
+    ...     'tab1': {
+    ...         'one': 1,
+    ...         'two': 2,
+    ...     },
+    ...     'tab2': {
+    ...         'three': 3,
+    ...     },
+    ...     'tab3': {
+    ...         'four': 4,
+    ...     },
+    ... }
+    >>> flatten = flatten_settings(settings)
+    >>> flatten == {'one': 1, 'two': 2, 'three': 3, 'four': 4}
+    True
+    """
     return {k: v for i in settings.values() for k, v in i.items()}
